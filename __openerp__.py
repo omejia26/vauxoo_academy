67145bc5ae0a9bbafba66ae66e6af3a134a1fb69
Omar <omejia@typrefrigeracion.com>
2016-05-05 11:49:00 -0700

    Primer commit estructura de un modulo

---
+++ __openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Open Academy",
+
+    'summary': """Primer modulo de entrenamiento vauxoo""",
+
+    'description': """
+        Open Academy module for managing trainings:
+    """,
+
+    'author': "omejia",
+    'website': "http://www.typrefrigeracion.com.mx",
+    'category': 'TyP_training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+    ],
+    'demo': [
+        # 'demo.xml',
+    ],
+}
