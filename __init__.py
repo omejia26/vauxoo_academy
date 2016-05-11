ea8b71ac8af47f7e6f6b91517e3b2025087774c7
omejia26 <omejia@typrefrigeracion.com>
2016-05-11 11:27:21 -0700

    [FIX] No poner comentarios antes de la importacion

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
-# Buena practica: utilizar from . para indicar la ubicacion de la carpeta
-from . import model
+# -*- encoding: utf-8 -*-
+from . import model  # Buena practica: utilizar from . para indicar la ubicacion de la carpeta
