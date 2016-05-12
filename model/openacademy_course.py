90dc73030f5f90fd16ccbf64873eb0625ce30514
omejia26 <omejia@typrefrigeracion.com>
2016-05-11 17:54:37 -0700

    [REF] Nombre y descripcion

--- model/openacademy_course.py
+++ model/openacademy_course.py
@@ -3,4 +3,11 @@ from openerp import models, fields
 
 
 class Course(models.Model):
-    _name = 'openacademy.course'
+    """
+      Esta clase crea el modelo del curso
+    """
+    _name = 'openacademy.course'  # Nombre de la tabla
+
+    name = fields.Char(string='Titulo', required=True)
+    description = fields.Text(string='Descripcion')
+
