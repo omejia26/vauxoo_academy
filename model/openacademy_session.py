260d747c73ed3f93e4c022c4f1ee3c5bdd672863
omejia26 <omejia@typrefrigeracion.com>
2016-05-17 10:22:08 -0700

    [REF] vauxoo_academy_project: Agregamos dominio e ilike

--- model/openacademy_session.py
+++ model/openacademy_session.py
@@ -9,7 +9,12 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duracion en dias")
     seats = fields.Integer(string="Numero de asientos")
-    instructor_id = fields.Many2one('res.partner', string="Instructor")
+    # El ilike busca en los tags tambien
+    instructor_id = fields.Many2one('res.partner', string="Instructor",
+                                    domain=['|',
+                                            ("instructor", "=", True,),
+                                            ("category_id.name", "ilike",
+                                             "Profesor")])
     course_id = fields.Many2one('openacademy.course',
                                 ondelete='cascade',
                                 string="Curso",
